cf05060eb85190c4f080ea79561d74fd44c5737f
AnaPinho1990 <anagonpinho@gmail.com>
2022-06-03 17:16:16 +0100

    login and signup showing but not posting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import IsAnon from "./components/IsAnon";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route
+          path="/signup"
+          element={
+            <IsAnon>
+              <SignupPage />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <IsAnon>
+              <LoginPage />
+            </IsAnon>
+          }
+        />
+      </Routes>
     </div>
   );
 }
---
+++ src/context/auth.context.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const API_URL = "http://localhost:3000";
+
+const AuthContext = React.createContext();
+
+function AuthProviderWrapper(props) {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState(null);
+
+  const storeToken = (token) => localStorage.setItem("authToken", token);
+  const removeToken = (token) => localStorage.removeItem("authToken");
+
+  const authenticateUser = () => {
+    const storedToken = localStorage.getItem("authToken");
+
+    if (storedToken) {
+      axios
+        .get(`${API_URL}/verify`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+        .then((response) => {
+          setIsLoggedIn(true);
+          setIsLoading(false);
+          setUser(response.data);
+        })
+        .catch((err) => {
+          setIsLoggedIn(false);
+          setIsLoading(false);
+          setUser(null);
+        });
+    } else {
+      setIsLoggedIn(false);
+      setIsLoading(false);
+      setUser(null);
+    }
+  };
+
+  const logoutUser = () => {
+    removeToken();
+    authenticateUser();
+  };
+
+  useEffect(() => {
+    authenticateUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        isLoading,
+        user,
+        storeToken,
+        authenticateUser,
+        logoutUser,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+}
+
+export { AuthContext, AuthProviderWrapper };
