e8396bcf204d8fbf6d9edf04b87e30881e02ede6
Catarina Salvado <catarinapsalvado@gmail.com>
2022-06-03 17:52:44 +0100

    added mondifications

--- src/App.js
+++ src/App.js
@@ -7,7 +7,25 @@ import IsAnon from "./components/IsAnon";
 function App() {
   return (
     <div className="App">
-   
+     {/*  <Navbar /> */}
+      <Routes>
+        <Route
+          path="/signup"
+          element={
+            <IsAnon>
+              <SignupPage />
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <IsAnon>
+              <LoginPage />
+            </IsAnon>
+          }
+        />
+      </Routes>
     </div>
   );
 }
